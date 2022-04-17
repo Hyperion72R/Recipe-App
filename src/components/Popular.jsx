006ae0131a9c27b406ecff6e5cf8f1a42aa1b566
Hyperion <radoslaw.lewandowski10@gmail.com>
2022-04-17 19:20:27 +0200

    Add some css

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -27,6 +27,10 @@ function Popular() {
         <Splide
           options={{
             perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
           }}
         >
           {popular.map((recipe) => {
@@ -35,6 +39,7 @@ function Popular() {
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -94,10 +99,41 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    // left: 50%;
+    bottom: 0%;
+    transform: transalate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
