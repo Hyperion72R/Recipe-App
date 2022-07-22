d298bcb3be1a3bfe37956482bd4f6ed22ed8bb02
Hyperion <radoslaw.lewandowski10@gmail.com>
2022-07-22 22:58:40 +0200

    Add new component

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopSticks } from "react-icons/gi";
+import styled from "styled-components";
+
+function Category() {
+  return (
+    <div>
+      <div>
+        <FaPizzaSlice>
+          <h4></h4>
+        </FaPizzaSlice>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
