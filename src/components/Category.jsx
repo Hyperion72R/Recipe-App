1c0e54f7811679005e0a03e31de3dfad7f5a11d0
Hyperion <radoslaw.lewandowski10@gmail.com>
2022-11-29 23:54:11 +0100

    Style NavLink

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,25 +6,25 @@ import { NavLink } from "react-router-dom";
 function Category() {
   return (
     <List>
-      <NavLink to={"/cousine/Italian"}>
+      <SLink to={"/cousine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={"/cousine/American"}>
+      <SLink to={"/cousine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={"/cousine/Thai"}>
+      <SLink to={"/cousine/Thai"}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
+      </SLink>
 
-      <NavLink to={"/cousine/Japanese"}>
+      <SLink to={"/cousine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </SLink>
     </List>
   );
 }
@@ -33,6 +33,31 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
 `;
 
 export default Category;
