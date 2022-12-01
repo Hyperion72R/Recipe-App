25dab77a74916a950fb3c123a7b6c6186f835420
Hyperion <radoslaw.lewandowski10@gmail.com>
2022-12-01 20:48:16 +0100

    Add new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import Search from "./components/Search";
 import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Category></Category>
+        <Routes>
+          <Route path="/" element={<Search />} />
+        </Routes>
         <Pages></Pages>
       </BrowserRouter>
     </div>
