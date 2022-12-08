f346ee2f8a148ad47babcbda8cf967127f241286
Hyperion <radoslaw.lewandowski10@gmail.com>
2022-12-08 22:48:08 +0100

    Modify the Searched component

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Searched() {
-  return <div>Searched</div>;
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+
+  let params = useParams;
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+    console.log(recipes);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+    console.log(params);
+    console.log(params.search);
+  }, [params.search]);
+
+  return (
+    <div>
+      {searchedRecipes.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.image} />
+            <h4>{item.title}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Searched;
