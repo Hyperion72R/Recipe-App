2767737d04de55c957c7df1b23d49a4b76f60d1f
Hyperion <radoslaw.lewandowski10@gmail.com>
2023-01-11 23:51:50 +0100

    complete animations

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,16 +3,20 @@ import Home from "./Home";
 import Cousine from "./Cousine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cousine/:type" element={<Cousine />} />
-      <Route path="/searched/:search" element={<Searched></Searched>} />
-      <Route path="/recipe/:name" element={<Recipe></Recipe>} />
-    </Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cousine/:type" element={<Cousine />} />
+        <Route path="/searched/:search" element={<Searched></Searched>} />
+        <Route path="/recipe/:name" element={<Recipe></Recipe>} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
